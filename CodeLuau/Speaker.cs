96c6c72d3f9306a8f3892f2a4871c7758fe08602
Rania.Gamal <rania.gamal@integrant.com>
2021-05-17 15:28:45 +0200

    remove useless comments

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -27,15 +27,11 @@ namespace CodeLuau
 		/// <returns>speakerID</returns>
 		public RegisterResponse Register(IRepository repository)
 		{
-			// lets init some vars
 			int? speakerId = null;
 			bool good = false;
 			bool appr = false;
-			//var nt = new List<string> {"Node.js", "Docker"};
 			var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
 
-			//DEFECT #5274 DA 12/10/2012
-			//We weren't filtering out the prodigy domain so I added it.
 			var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
 
 			if (!string.IsNullOrWhiteSpace(FirstName))
@@ -44,14 +40,12 @@ namespace CodeLuau
 				{
 					if (!string.IsNullOrWhiteSpace(Email))
 					{
-						//put list of employers in array
 						var emps = new List<string>() { "Pluralsight", "Microsoft", "Google" };
 
 						good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
 
 						if (!good)
 						{
-							//need to get just the domain from the email
 							string emailDomain = Email.Split('@').Last();
 
 							if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
@@ -66,14 +60,6 @@ namespace CodeLuau
 							{
 								foreach (var session in Sessions)
 								{
-									//foreach (var tech in nt)
-									//{
-									//    if (session.Title.Contains(tech))
-									//    {
-									//        session.Approved = true;
-									//        break;
-									//    }
-									//}
 
 									foreach (var tech in ot)
 									{
@@ -97,10 +83,6 @@ namespace CodeLuau
 
 							if (appr)
 							{
-								//if we got this far, the speaker is approved
-								//let's go ahead and register him/her now.
-								//First, let's calculate the registration fee. 
-								//More experienced speakers pay a lower fee.
 								if (Exp <= 1)
 								{
 									RegistrationFee = 500;
@@ -123,14 +105,12 @@ namespace CodeLuau
 								}
 
 
-								//Now, save the speaker and sessions to the db.
 								try
 								{
 									speakerId = repository.SaveSpeaker(this);
 								}
 								catch (Exception e)
 								{
-									//in case the db call fails 
 								}
 							}
 							else
@@ -158,7 +138,6 @@ namespace CodeLuau
 				return new RegisterResponse(RegisterError.FirstNameRequired);
 			}
 
-			//if we got this far, the speaker is registered.
 			return new RegisterResponse((int)speakerId);
 		}
 	}
